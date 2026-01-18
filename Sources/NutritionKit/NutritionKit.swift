1998e8853e116c8189a6c26450cf6602aea5cf07
Timo Virtaneva <timo@virtaneva.com>
2026-01-18 21:09:22 +0200

    Adding Logger

--- Sources/NutritionKit/NutritionKit.swift
+++ Sources/NutritionKit/NutritionKit.swift
@@ -4,3 +4,13 @@ import Toolbox
 extension Log {
     static let nutritionKit = Logger(subsystem: "com.jonaszell.NutritionKit", category: "NutritionKit")
 }
+
+struct Logger {
+    init(subsystem: String, category: String) {}
+    func log(_ message: String) {}
+    func info(_ message: String) {}
+    func debug(_ message: String) {}
+    func error(_ message: String) {}
+    // Add other log levels as needed
+}
+
